dd5bd992372fb7c7458ae93ed11d95270fc58252
Zolto Zoltomon <zdorzhyiev@gmail.com>
2024-06-23 23:37:24 +0300

    Юнит тест

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("348fc02d-be3a-455d-9a9b-7241b1813ec8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Test5.Controllers;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException (typeof(Exception))]
+        public void TestMethod1()
+        {
+            AutorizationController controller = new AutorizationController();
+            controller.Autorization("", "");
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            AutorizationController controller1 = new AutorizationController();
+            controller1.Autorization("Test2", "Test2");
+        }
+    }
+}
